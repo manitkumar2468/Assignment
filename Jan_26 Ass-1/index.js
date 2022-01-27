b6581dc83ba9834c9866829b88e5c0f91fc7b7d1
manitkumar2468 <manitkumar1998@gmail.com>
2022-01-27 17:09:33 +0530

    Add files via upload

---
+++ Jan_26 Ass-1/index.js
@@ -0,0 +1,14 @@
+var a=prompt("Enter number 1");
+var b=prompt("Enter number 2");
+var c=prompt("Enter number 3");
+
+if(a<=b && a<=c){
+prompt(" 1st number is minimum = " + a);
+}
+else if(b<=a && b<=c){
+prompt(" 2nd number is minimum = " + b);
+}
+
+else{
+prompt(" 3rd number is minimum = " + c);
+}
