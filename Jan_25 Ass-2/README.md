2291b1ed43af9facb397bac79d10a3a53ebd8f94
manitkumar2468 <manitkumar1998@gmail.com>
2022-01-30 15:47:02 +0530

    Delete Jan_25 Ass-2 directory

--- Jan_25 Ass-2/README.md
+++
@@ -1,1 +0,0 @@
-Display maximum of two number
--- Jan_25 Ass-2/hello.js
+++
@@ -1,9 +0,0 @@
-var a=10, b=30;
-
-if(a>=b){
-console.log("1st number is greater = " + a);
-}
-
-else{
-console.log("2nd number is greater = " + b);
-}
